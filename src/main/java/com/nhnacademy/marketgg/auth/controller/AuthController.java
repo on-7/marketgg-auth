cdc362985ae4af6310df7f89b2a2c150c50b3939
corock <hoongineer@gmail.com>
2022-07-14 15:06:37 +0900

    Docs: 컨트롤러 패키지 하위 클래스 Javadoc 작성 #14

--- src/main/java/com/nhnacademy/marketgg/auth/controller/AuthController.java
+++ src/main/java/com/nhnacademy/marketgg/auth/controller/AuthController.java
@@ -1,14 +1,13 @@
 package com.nhnacademy.marketgg.auth.controller;
 
+import com.nhnacademy.marketgg.auth.dto.request.EmailRequest;
 import com.nhnacademy.marketgg.auth.dto.request.LoginRequest;
+import com.nhnacademy.marketgg.auth.dto.request.SignupRequest;
 import com.nhnacademy.marketgg.auth.dto.response.EmailResponse;
 import com.nhnacademy.marketgg.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-
-import com.nhnacademy.marketgg.auth.dto.request.EmailRequest;
-import com.nhnacademy.marketgg.auth.dto.request.SignupRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.management.relation.RoleNotFoundException;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @Slf4j
 @RestController
@@ -28,25 +28,48 @@ public class AuthController {
 
     private final AuthService authService;
 
+    /**
+     * 로그인 요청을 받아 로그인을 진행합니다.
+     *
+     * @param loginRequest - 로그인에 필요한 요청 정보 객체
+     * @return 로그인 성공/실패 여부가 담긴 ResponseEntity
+     */
     @PostMapping("/login")
     public ResponseEntity<Void> doLogin(@RequestBody LoginRequest loginRequest) {
         String token = authService.login(loginRequest);
+
         return ResponseEntity.status(HttpStatus.OK)
                              .headers(header -> header.setBearerAuth(token))
                              .build();
     }
 
+    /**
+     * 회원가입 요청을 받아 회원가입을 진행합니다.
+     *
+     * @param signupRequest - 회원가입에 필요한 요청 정보 객체
+     * @return 회원가입 성공/실패 여부가 담긴 ResponseEntity
+     * @throws RoleNotFoundException - 역할을 부여받지 않거나, 읽을 수 없는 경우 예외 발생
+     */
     @PostMapping("/signup")
     public ResponseEntity<Void> doSignup(@RequestBody final SignupRequest signupRequest) throws RoleNotFoundException {
         authService.signup(signupRequest);
+
         return ResponseEntity.status(CREATED)
                              .build();
     }
 
+    /**
+     * 요청한 이메일이 유효한지 확인합니다.
+     *
+     * @param emailRequest - 이메일 로그인 요청 정보 객체
+     * @return 이메일로 로그인 요청 성공/실패 여부가 담긴 ResponseEntity
+     * @throws Exception - 예외 발생
+     */
     @PostMapping("/check/email")
     public ResponseEntity<EmailResponse> checkEmail(@RequestBody EmailRequest emailRequest) throws Exception {
         return ResponseEntity.status(OK)
                              .contentType(MediaType.APPLICATION_JSON)
                              .body(authService.checkEmail(emailRequest.getEmail()));
     }
+
 }
