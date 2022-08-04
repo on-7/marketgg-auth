f4365775d774a9256deffbba6d2cfe5a07f859eb
Dongyeol <eastheat10@gmail.com>
2022-08-04 20:26:24 +0900

    Docs: JavaDoc 추가

--- src/main/java/com/nhnacademy/marketgg/auth/adapter/OauthAdapter.java
+++ src/main/java/com/nhnacademy/marketgg/auth/adapter/OauthAdapter.java
@@ -13,8 +13,23 @@ import org.springframework.util.MultiValueMap;
  */
 public interface OauthAdapter {
 
+    /**
+     * 소셜 로그인 벤더에 Access Token 을 요청합니다.
+     *
+     * @param requestUrl - 요청 URL
+     * @param params     - 요청 시 필요한 Parameter (client id, client secret, ...)
+     * @return Access Token
+     */
     ResponseEntity<OAuthToken> requestToken(String requestUrl, HttpEntity<MultiValueMap<String, String>> params);
 
+    /**
+     * Access Token 으로 사용자의 프로필 정보를 요청합니다.
+     *
+     * @param requestUri - 사용자 프로필을 요청할 URI
+     * @param clazz      - 반환받을 클래스 타입
+     * @param <T>        - 소셜로그인 벤더
+     * @return 사용자 프로필
+     */
     <T> ResponseEntity<T> requestProfile(URI requestUri, Class<T> clazz);
 
 }
